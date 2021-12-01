28611d464514c822563cd78a40f19e959b2b59d6
ylu <ylu@ert.com>
2021-12-01 14:45:05 -0500

    update  game over for collision with tail.

--- main.py
+++ main.py
@@ -41,6 +41,6 @@ while game_is_on:
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
-        scoreboard.game_over()
+            scoreboard.game_over()
 
 screen.exitonclick()
